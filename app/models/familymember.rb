0065806f994c915ac64013af964bbb5e35665824
Steven Rayesky <Protolexus@gmail.com>
2016-02-17 11:02:21 -0500

    Completed basic form creating a new familymember on familymembers#show. Closed #5.

--- app/models/familymember.rb
+++ app/models/familymember.rb
@@ -1,11 +1,17 @@
 class Familymember < ActiveRecord::Base
 	has_many :familymembers
-	has_many :relatives
 	belongs_to :user
-	has_many :parents, through: :relatives, source: :familymember, foreign_key: "child_id"
-	has_many :children, through: :relatives, source: :familymember, foreign_key: "parent_id"
-	has_many :wives, through: :marriages, source: :familymember, foreign_key: "husband_id"
-	has_many :husbands, through: :marriages, source: :familymember, foreign_key: "wife_id"
+	has_many :parent_relatives, class_name: "Relative", foreign_key: "child_id"
+	has_many :child_relatives, class_name: "Relative", foreign_key: "parent_id"
+
+	has_many :parents, through: :parent_relatives, source: :parent 
+	has_many :children, through: :child_relatives, source: :child
+
+	has_many :wife_marriages, class_name: "Marriage", foreign_key: "husband_id"
+	has_many :husband_marriages, class_name: "Marriage", foreign_key: "wife_id"
+
+	has_many :wives, through: :wife_marriages, source: :bride
+	has_many :husbands, through: :husband_marriages, source: :groom
 
 
 end
--- app/models/marriage.rb
+++ app/models/marriage.rb
@@ -1,5 +1,6 @@
 class Marriage < ActiveRecord::Base
 
-	belongs_to :familymember
+	belongs_to :groom, class_name: "Familymember"
+	belongs_to :bride, class_name: "Familymember"
 	
 end
