0cd86e3983ca244d75980056d0c13058d5625912
Steven Rayesky <Protolexus@gmail.com>
2016-02-19 10:58:07 -0500

    familymember edit and destroy functioning. Closed #17. Closed #16.

--- app/controllers/familymembers_controller.rb
+++ app/controllers/familymembers_controller.rb
@@ -6,7 +6,7 @@ class FamilymembersController < ApplicationController
 
 	def create
 		current_user
-		@familymember = Familymember.new(member_params)
+		@familymember = Familymember.new(familymember_params)
 		@familymember.tree_id = params[:tree_id]
 		if @familymember.save
 			redirect_to (:back)
@@ -17,12 +17,24 @@ class FamilymembersController < ApplicationController
 		current_user
 	end
 
+	def update
+		@familymember = Familymember.find(params[:id])
+		@familymember.update(familymember_params)
+		if @familymember.save
+			redirect_to (:back)
+		end
+	end
 
+	def destroy
+		@familymember = Familymember.find(params[:id])
+		if @familymember.destroy
+			redirect_to (:back)
+		end
+	end
 
 	private
 
-	def member_params
+	def familymember_params
 		params.require(:familymember).permit(:first_name, :last_name, :birthday, :death, :birth_loc, :death_loc)
-		
 	end
 end
